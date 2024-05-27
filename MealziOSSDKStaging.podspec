1a44327a464426fb0d0412f6eee3a7d481c2d573
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-27 08:28:20 +0200

    update release

---
+++ MealziOSSDKStaging.podspec
@@ -0,0 +1,17 @@
+
+Pod::Spec.new do |spec|
+    spec.name              = 'MealziOSSDKStaging'
+    spec.version           = '1.0.2'
+    spec.summary           = 'Miam Core for iOS'
+    spec.homepage          = 'https://www.miam.tech'
+       spec.description           = <<-DESC
+       Mealz MealziOSSDK SDK for iOS.
+       DESC
+    spec.author            = { 'Diarmuid McGonagle, Damien Walerowicz' => 'it@miam.tech' }
+    spec.license           = { :type => 'GPLv3', :file => 'LICENSE' }
+    spec.swift_versions = "5.8"
+    spec.platform          = :ios, "12.0"
+    spec.source            = { :http => 'https://github.com/MealzStaging/MealziOSSDKRelease/raw/release/1.0.2/MealziOSSDK.zip' }
+    spec.dependency 'MealzCoreStaging', '~> 3.0.19'
+    spec.ios.vendored_frameworks = 'MealziOSSDK.xcframework'
+end
---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let configurationMode = "prod"
+
+let package = Package(
+    name: "MealziOSSDK",
+    defaultLocalization: "fr",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "MealziOSSDK",
+            targets: ["MealziOSSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/miamtech/MealzCoreRelease", from: "1.0.13")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MealziOSSDK",
+            url: "https://github.com/MealzStaging/MealziOSSDKRelease/raw/release/1.0.2/MealziOSSDK.zip",
+            checksum: "4ff47511b6d776d865ebaaaccfa66cfeb1a67c0a97de9936fac6e3cda095fa5f"
+        )
+        
+    ]
+)
