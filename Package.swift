db013b9403e5b705c23a0092a166fb88477c17ca
Thomas Lemay <thomas.lemay@miam.tech>
2024-08-14 15:19:09 +0200

    update release

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let configurationMode = "prod"
+
+let package = Package(
+    name: "MealziOSSDK",
+    defaultLocalization: "fr",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "MealziOSSDK",
+            targets: ["MealziOSSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MealzStaging/MealzCoreRelease", from: "0.0.1"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MealziOSSDK",
+            url: "https://github.com/MealzStaging/MealziOSSDKRelease/raw/release/4.1.0/MealziOSSDK.zip",
+            checksum: "0574a809ef371c26dce57d7ef243a78c3b750c8ec7b694a5ceea5568fbf589dc"
+        ),
+    ]
+)
